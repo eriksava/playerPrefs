aef298560469b2b74c8d990ecd50aa062b83078c
Erik S <eriksava@hotmail.com>
2018-11-02 18:34:51 -0700

    base project

---
+++ Assets/PlayerPrefs/PlayerPrefsExample.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefsExample : MonoBehaviour {
+
+
+    public Settings playerSettings;
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.S)) // SAVE
+        {
+            SaveSettings(playerSettings);
+            Debug.Log("SAVE DSETTINGS");
+        }
+
+        if (Input.GetKeyDown(KeyCode.L)) // LOAD
+        {
+            playerSettings = LoadSettings();
+            Debug.Log("LOAD SETTINGS");
+        }
+        if (Input.GetKeyDown(KeyCode.D)) // DELETE ALL
+        {
+            PlayerPrefs.DeleteAll();
+            Debug.Log("DELETE ALL");
+        }
+
+    }
+
+    void SaveSettings(Settings settings){
+
+        PlayerPrefs.SetString("AXIS", settings.axisname);
+        PlayerPrefs.SetFloat("VOLUME", settings.volume);
+        PlayerPrefs.SetInt("DIFFICULTY", (int)settings.difficulty);
+
+    }
+
+
+    Settings LoadSettings(){
+
+        var resultSettings = new Settings();
+
+        resultSettings.axisname = PlayerPrefs.GetString("AXIS", "Horizontal");
+        resultSettings.volume = PlayerPrefs.GetFloat("VOLUME", 5F);
+        resultSettings.difficulty = (Difficulty)PlayerPrefs.GetInt("DIFFICULTY", 0);
+
+        return resultSettings;
+
+    }
+}
---
+++ Assets/PlayerPrefs/Settings.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+
+public class Settings{
+
+    public string axisname = "Horizontal";
+    public float volume = 5f;
+    public Difficulty difficulty;
+
+
+}
+
+public enum Difficulty{
+
+    EASY,
+    MEDIUM,
+    HARD,
+    INSANE
+
+}
